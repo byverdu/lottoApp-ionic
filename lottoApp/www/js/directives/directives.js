d2f3ab13da13af0cd986457090e0df36a971cec7
byverdu <byverdu@gmail.com>
2016-10-16 23:49:24 +0100

    moved directives templates into own folder and created a group btns directive

--- lottoApp/www/js/directives/directives.js
+++ lottoApp/www/js/directives/directives.js
@@ -4,11 +4,27 @@ angular.module( 'lottoApp.directives', [])
   .directive( 'cardBall', function () {
     return {
       restrict: 'E',
-      templateUrl: '../../templates/card-ball.html',
+      templateUrl: '../../templates/components/card-ball.html',
       scope: {
         title: '@',
         cardBalls: '=',
         isToggleCard: '='
+      }
+    };
+  })
+  .directive( 'actionButtons', function () {
+    return {
+      restrict: 'E',
+      templateUrl: '../../templates/components/action-buttons.html',
+      scope: {
+        firstCall: '&',
+        firstText: '@',
+        secondCall: '&',
+        secondText: '@',
+        thirdCall: '&',
+        thirdText: '@',
+        countChecked: '&',
+        countBalls: '='
       }
     };
   });
