92323225c44174ef7e497e526fdbdbc903f7ecb3
byverdu <byverdu@gmail.com>
2016-10-23 00:46:16 +0100

    lazy

---
+++ lottoApp/www/js/controllers/StatsCtrl.js
@@ -0,0 +1,30 @@
+/* global angular */
+
+angular.module( 'lottoApp.controllers' )
+  .controller( 'StatsCtrl', function (
+    $scope,
+    sharedData,
+    utilsService
+  ) {
+    const statsCtrl = $scope;
+    statsCtrl.raffle = sharedData.getData();
+    statsCtrl.addStringZero = utilsService.addStringZero;
+
+    statsCtrl.sortByIndexOrCount = function ( type ) {
+      const stats = statsCtrl.raffle.data.statistics;
+      switch ( type ) {
+        case 'index':
+          return stats.sort(
+            ( a, b ) => a.index - b.index
+          );
+        case 'count':
+          return stats.sort(
+            ( a, b ) => b.count - a.count
+          );
+        default:
+          return stats.sort(
+            ( a, b ) => a.index - b.index
+          );
+      }
+    };
+  });
